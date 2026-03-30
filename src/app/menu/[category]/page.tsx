fc1862c963fea39aa5ee7418e3781a7078484ea9
Vishal Mishra <vishalmishra@medianv.com>
2026-03-30 14:20:33 +0530

    added all the routes and creted the basic strcture of the app

---
+++ src/app/menu/[category]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoryPage = () => {
+  return (
+    <div>
+      Catogery page
+    </div>
+  )
+}
+
+export default CategoryPage
---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SingleProductPage = () => {
+  return (
+    <div>
+        SingleProductPage
+      
+    </div>
+  )
+}
+
+export default SingleProductPage;
