11f0479b21486283f8f3a60cfb6aeae5d1533309
Vishal Mishra <vishalmishra@medianv.com>
2026-03-30 14:54:01 +0530

    added components

---
+++ src/components/Menu.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+        <Image src="/open.png"/>
+      
+    </div>
+  )
+}
+
+export default Menu
