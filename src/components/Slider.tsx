68a8efa6bd3abfda37dd70a65f44d33e24f8b9d1
Vishal Mishra <vishalmishra@medianv.com>
2026-03-31 12:09:42 +0530

    added the body image and caption for that

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
+
+const data = [
+  {
+    id:1,
+    title:"always fresh & always crsipy & always hot🔥",
+    image:"/slide1.png"
+  },
+  {
+    id:2,
+    title:"we deliver your order wherever you are in Ind",
+    image:"/slide2.png"
+  },
+  {
+    id:1,
+    title:"the best pizza to share with your family!!",
+    image:"/slide3.jpg"
+  }
+
+];
 
 const Slider = () => {
+  const[currentSlide, setCurrentSlide] = useState(0);
+  // useEffect(()=>{
+  //   const interval = setInterval(
+  //     ()=> setCurrentSlide((prev)=>prev === data.length-1 ?0 : prev+1), 2000);
+  //   return ()=> clearInterval(interval);
+  // },[]);
   return (
-    <div>
-      Slider
+    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50'>
+      {/* Text Container*/}
+      <div className='flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold'>
+        <h1 className='text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl'>{data[currentSlide].title}</h1>
+        <button className='bg-red-500 text-white py-4 px-8'>Order Now!!</button>
+      </div>
+      {/*Image Container*/}
+      <div className='w-full flex-1 relative'>
+        <Image src={data[currentSlide].image} alt="" fill/>
+
+      </div>
+      
     </div>
   )
 }
