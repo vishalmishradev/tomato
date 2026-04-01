7098fa86619be91e35a839e15e28bb53f6539808
Vishal Mishra <vishalmishra@medianv.com>
2026-04-01 13:49:14 +0530

    created add to card page

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,12 +1,13 @@
 import { pizzas } from '@/data'
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 const CategoryPage = () => {
   return (
     <div className='flex flex-wrap text-red-500'>
       {pizzas.map((item)=>(
-        <Link className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4' href="/product/">
+        <Link className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group even:bg-fuchsia-50' href={`/product/${item.id} key={item.id}`}>
         {/* Image container*/}
         {item.img && (
           <div className='relative h-[80%]'>
@@ -14,10 +15,10 @@ const CategoryPage = () => {
           </div>
         )}
         {/*Text container*/}
-        <div className='flex items-center justify-between font-bold'>
-          <h1>{item.title}</h1>
-          <h2>${item.price}</h2>
-          <button className='uppercase bg-red-500 text-white p-2 rounded-md'>Add to cart</button>
+        <div className='flex items-center justify-between font-bold '>
+          <h1 className='text-2xl uppercase p-2'>{item.title}</h1>
+          <h2 className='group-hover:hidden text-xl'>${item.price}</h2>
+          <button className='hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md'>Add to cart</button>
         </div>
         </Link>
       ))
---
+++ src/components/Price.tsx
@@ -0,0 +1,58 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+
+type prop ={
+    price:number,
+    id:number,
+    options?:{title:string;
+        additionalPrice: number
+    }[]
+}
+
+const Price = ({price, id, options}:prop) => {
+ 
+    const [total, setTotal] = useState(price);
+    const [quantity, setQuantity] = useState(1);
+    const [selected, setSelected] = useState(0);
+
+    useEffect(()=>{
+        setTotal(
+            quantity * (options ? price + options[selected].additionalPrice : price)
+        )
+
+    },[quantity, selected, options, price]);
+
+  return (
+    <div className='flex flex-col gap-4'>
+      <h2 className='font-bold text-2xl'>{total.toFixed(2)}</h2>
+      {/*Option container */}
+      <div className="flex gap-4">
+        {options?.map((option, index) =>(
+            <button key={option.title} className="min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md" 
+            style={{background:selected === index ? "rgb(248 113 113" : "white",
+                color:selected === index ? "white" : "red",
+            }}
+            onClick={()=>setSelected(index)}
+            >{option.title}</button>
+        ))}
+
+      </div>
+      {/*Quantity and add button container*/}
+      <div className="flex justify-between items-center">
+        {/*Quantity*/}
+        <div className="flex justify-between w-full p-3 ring-1 ring-red-500">
+            <span>Quantity</span>
+            <div className='flex gap-4 items-center'>
+                <button onClick={()=>setQuantity(prev => (prev>1 ?prev-1 : 1))}>{'<'}</button>
+                <span>{quantity}</span>
+                <button onClick={()=>setQuantity(prev =>(prev<9 ? prev+1 :9))}>{'>'}</button>
+            </div>
+        </div>
+        {/*Cart button*/}
+        <button className="uppercase w-56 bg-red-500 text-white p-3 ring1 ring-red-500">Add to cart</button>
+      </div>
+    </div>
+  )
+}
+
+export default Price
