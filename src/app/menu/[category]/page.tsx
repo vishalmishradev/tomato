e94b3a801c5cb9ce48ebbbf3c8d70395c25f3601
Vishal Mishra <vishalmishra@medianv.com>
2026-04-01 11:22:28 +0530

    added catogary items

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,9 +1,29 @@
+import { pizzas } from '@/data'
+import Link from 'next/link'
 import React from 'react'
 
 const CategoryPage = () => {
   return (
-    <div>
-      Catogery page
+    <div className='flex flex-wrap text-red-500'>
+      {pizzas.map((item)=>(
+        <Link className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4' href="/product/">
+        {/* Image container*/}
+        {item.img && (
+          <div className='relative h-[80%]'>
+          <Image src={item.img} alt="" fill className="object-contain"/>
+          </div>
+        )}
+        {/*Text container*/}
+        <div className='flex items-center justify-between font-bold'>
+          <h1>{item.title}</h1>
+          <h2>${item.price}</h2>
+          <button className='uppercase bg-red-500 text-white p-2 rounded-md'>Add to cart</button>
+        </div>
+        </Link>
+      ))
+
+      }
+     
     </div>
   )
 }
