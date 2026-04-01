d11ce1801e79c11078a8b4dbcb88104bde470a38
Vishal Mishra <vishalmishra@medianv.com>
2026-04-01 15:54:08 +0530

    added dymanic date

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -2,11 +2,15 @@
 import React from 'react'
 import Countdown from 'react-countdown';
 
-const endingDate = new Date("2026-04-01");
+// Dynamic: always 24 hours from now
+const endingDate = new Date(Date.now() + 24 * 60 * 60 * 1000);
 
 const CountDown = () => {
   return (
-    <Countdown className='font-bold text-5xl text-yellow-300' date={endingDate} />
+    <Countdown 
+      className='font-bold text-5xl text-yellow-300' 
+      date={endingDate} 
+    />
   )
 }
 
